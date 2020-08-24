175c6b95b1c2f6b2cb1f2e98ae8567e5c07c676c
Nam <sjarbgud@gmail.com>
2020-08-24 23:35:11 +0900

    (DAY1)CLASS04 - 의존성 주입

--- BoardWeb/src/main/java/polymorphism/LgTV.java
+++ BoardWeb/src/main/java/polymorphism/LgTV.java
@@ -1,18 +1,23 @@
 package polymorphism;
 
 public class LgTV implements TV{
-//	public void turnOn() {
-//		System.out.println("LgTV --- 전원 켠다.");
-//	}
-//	public void turnOff() {
-//		System.out.println("LgTV --- 전원 끈다.");
-//	}
-//	public void soundUp() {
-//		System.out.println("LgTV --- 소리 올린다.");
-//	}
-//	public void soundDown() {
-//		System.out.println("LgTV --- 소리 내린다.");
-//	}
+	private Speaker speaker;
+	private int price;
+	
+	public LgTV() {
+		System.out.println("===> LgTV(1) 객체 생성");
+	}
+	
+	public void setSpeaker(Speaker speaker) {
+		System.out.println("===> setSpeaker() 호출");
+		this.speaker = speaker;
+	}
+	
+	public void setPrice(int price) {
+		System.out.println("===> setPrice() 호출");
+		this.price = price;
+	}
+	
 	@Override
 	public void powerOn() {
 		// TODO Auto-generated method stub
@@ -26,11 +31,13 @@ public class LgTV implements TV{
 	@Override
 	public void volumeUp() {
 		// TODO Auto-generated method stub
-		System.out.println("LgTV --- 소리 올린다.");
+//		System.out.println("LgTV --- 소리 올린다.");
+		speaker.volumeUp();
 	}
 	@Override
 	public void volumeDown() {
 		// TODO Auto-generated method stub
-		System.out.println("LgTV --- 소리 내린다.");
+//		System.out.println("LgTV --- 소리 내린다.");
+		speaker.volumeDown();
 	}	
 }
--- BoardWeb/src/main/java/polymorphism/TVUser.java
+++ BoardWeb/src/main/java/polymorphism/TVUser.java
@@ -34,15 +34,15 @@ public class TVUser {
 //		GenericXmlApplicationContext 객체가 생성되어 스프링 컨테이너가 구동됐다는 메세지가 출력
 		
 		// 2. Spring 컨테이너로부터 필요한 객체를 요청(Lookup) 한다.
-//		TV tv = (TV)factory.getBean("tv");
-//		tv.powerOn();
-//		tv.volumeUp();
-//		tv.volumeDown();
-//		tv.powerOff();
+		TV tv = (TV)factory.getBean("LgTV");
+		tv.powerOn();
+		tv.volumeUp();
+		tv.volumeDown();
+		tv.powerOff();
 		
-		TV tv1 = (TV)factory.getBean("tv");
-		TV tv2 = (TV)factory.getBean("tv");
-		TV tv3 = (TV)factory.getBean("tv");
+//		TV tv1 = (TV)factory.getBean("tv");
+//		TV tv2 = (TV)factory.getBean("tv");
+//		TV tv3 = (TV)factory.getBean("tv");
 		// => 싱글톤으로 설정해서 클라이언트에서 세번 요청해도 객체는 메모리에 하나만 생성되어 유지된다. <-> prototype (스프링 컨테이너가 해당 빈을 요청할 때 마다 매번 새로운 객체 생성해서 반환)
 		
 		
