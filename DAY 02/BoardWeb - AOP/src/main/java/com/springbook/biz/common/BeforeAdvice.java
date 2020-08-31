b17f9dfc12928a0bc35e7b0fa43ae6bfd7490243
Nam <sjarbgud@gmail.com>
2020-09-01 00:23:33 +0900

    (DAY2)CLASS04 - JoinPoint와 바인드 변수

--- DAY 02/BoardWeb - AOP/src/main/java/com/springbook/biz/common/BeforeAdvice.java
+++ DAY 02/BoardWeb - AOP/src/main/java/com/springbook/biz/common/BeforeAdvice.java
@@ -1,7 +1,12 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.JoinPoint;
+
 public class BeforeAdvice {
-	public void beforeLog() {
-		System.out.println("[사전 처리] 비즈니스 로직 수행 전 동작");
+	public void beforeLog(JoinPoint jp) {
+		String method = jp.getSignature().getName();	//JoinPoint의 getSignature() 메소드를 이용하여 클라이언트가 호출한 메소드 이름을 출력할 수 있다.
+		Object[] args = jp.getArgs();	// getArgs() 메소드를 통해 인자 목록을 Object 배열로 얻을수 있다.
+		
+		System.out.println("[사전 처리] " + method + "() 메소드 ARGS 정보 : " + args[0].toString());
 	}
 }
