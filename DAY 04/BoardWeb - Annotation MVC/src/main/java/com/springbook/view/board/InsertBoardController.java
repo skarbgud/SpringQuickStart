90dad98702880788ea1bff413a82faa6be08c38a
Nam <sjarbgud@gmail.com>
2020-09-08 00:06:25 +0900

    (DAY4)CLASS02 - 어노테이션으로 게시판 프로그램 구현하기(2.7로그아웃 기능 구현하기 까지)

--- DAY 04/BoardWeb - Annotation MVC/src/main/java/com/springbook/view/board/InsertBoardController.java
+++ DAY 04/BoardWeb - Annotation MVC/src/main/java/com/springbook/view/board/InsertBoardController.java
@@ -1,11 +1,7 @@
 package com.springbook.view.board;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
@@ -14,23 +10,10 @@ import com.springbook.biz.board.impl.BoardDAO;
 public class InsertBoardController{
 	
 	@RequestMapping(value = "/insertBoard.do")
-	public void insertBoard(BoardVO vo) {
-		System.out.println("글 등록 처리");
-		
-		// 1. 사용자 입력 정보 추출			
-//		String title = request.getParameter("title");
-//		String writer = request.getParameter("writer");
-//		String content = request.getParameter("content");
-		
-		// 2. DB 연동 처리
-//		BoardVO vo = new BoardVO();
-//		vo.setTitle(title);
-//		vo.setWriter(writer);
-//		vo.setContent(content);
-		
-		BoardDAO boardDAO = new BoardDAO();
+	public String insertBoard(BoardVO vo, BoardDAO boardDAO) {
 		boardDAO.insertBoard(vo);
-
+		return "getBoardList.do";
+		
 		/*
 		  	서블릿 객체의 service() 메소드가 호출되는 과정
 		  	1. 서블릿 컨테이너는 클라이언트의 HTTP 요청이 서버에 전달되는 순간
