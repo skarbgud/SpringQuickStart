e7dffee300d46343c5e9d657ab8d540334813bce
Nam <sjarbgud@gmail.com>
2020-09-14 00:12:06 +0900

    (DAY5)CLASS01 - Mybatis 프레임워크 시작하기

---
+++ DAY 05/MybatisProject/src/main/java/com/springbook/biz/board/BoardVO.java
@@ -0,0 +1,94 @@
+package com.springbook.biz.board;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlTransient;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+// VO(Value Object)
+@XmlAccessorType(XmlAccessType.FIELD) // @XmlAccessorType는 BoardVO객체를 XML로 변환할 수 있다는 의미  	//XmlAccessType.FIELD 변수들은 자독으로 자식 엘리먼트로 표현
+public class BoardVO {
+	@XmlAttribute	// seq를 속성으로 표현하라는 의미
+	private int seq;
+	private String title;
+	private String writer;
+	private String content;
+	private Date regDate;
+	private int cnt;
+	@XmlTransient	// @XmlTransient XML 변환에서 제외하라는 의미 = @JsonIgnore
+	private String searchCondition;
+	@XmlTransient
+	private String searchKeyword;
+	@XmlTransient
+	private MultipartFile uploadFile;
+	
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public int getCnt() {
+		return cnt;
+	}
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+	@JsonIgnore
+	public String getSearchCondition() {
+		return searchCondition;
+	}
+	public void setSearchCondition(String searchCondition) {
+		this.searchCondition = searchCondition;
+	}
+	@JsonIgnore
+	public String getSearchKeyword() {
+		return searchKeyword;
+	}
+	public void setSearchKeyword(String searchKeyword) {
+		this.searchKeyword = searchKeyword;
+	}
+	@JsonIgnore
+	public MultipartFile getUploadFile() {
+		return uploadFile;
+	}
+	public void setUploadFile(MultipartFile uploadFile) {
+		this.uploadFile = uploadFile;
+	}
+	@Override
+	public String toString() {
+		return "BoardVO [seq=" + seq + ", title=" + title + ", writer=" + writer + ", content=" + content + ", regDate="
+				+ regDate + ", cnt=" + cnt + "]";
+	}
+	
+}
---
+++ DAY 05/MybatisProject/src/main/java/com/springbook/biz/util/SqlSessionFactoryBean.java
@@ -0,0 +1,26 @@
+package com.springbook.biz.util;
+
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class SqlSessionFactoryBean {
+	private static SqlSessionFactory sessionFactory = null;
+	static {
+		try {
+			if(sessionFactory == null) {
+				Reader reader = Resources.getResourceAsReader("sql-map-config.xml");	// sql-map-config.xml 입력 스트림을 생성
+				sessionFactory = new SqlSessionFactoryBuilder().build(reader);	// 입력 스트림을 통해 sql-map-config.xml 파일 읽어 SqlSessionFactory 객체 생성 
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static SqlSession getSqlSessionInstace() {
+		return sessionFactory.openSession();
+	}
+}
