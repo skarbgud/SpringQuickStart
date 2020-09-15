559c298ed5726ee65ac55213f133bd74b91d9ce6
Nam <sjarbgud@gmail.com>
2020-09-15 21:26:33 +0900

    (DAY5)CLASS02 - Mapper XML 파일 설정

--- DAY 05/MybatisProject/src/main/java/com/springbook/biz/board/impl/BoardDAO.java
+++ DAY 05/MybatisProject/src/main/java/com/springbook/biz/board/impl/BoardDAO.java
@@ -27,13 +27,17 @@ public class BoardDAO{
 	
 	public void deleteBoard(BoardVO vo) {
 		mybatis.delete("BoardDAO.deleteBoard", vo);
+		mybatis.commit();
 	}
 
 	public BoardVO getBoard(BoardVO vo) {
+		// selectOne() 메소드는 오직 하나의 데이터를 검색하는 SQL 구문
+		// selectOne("XML네임스페이스.id", 파라미터 정보)
 		return (BoardVO) mybatis.selectOne("BoardDAO.getBoard", vo);
 	}
 	
 	public List<BoardVO> getBoardList(BoardVO vo){
+		// selectList() 여러 개의 데이터 검색
 		return mybatis.selectList("BoardDAO.getBoardList", vo);
 	}
 }
--- DAY 05/MybatisProject/src/main/java/com/springbook/biz/util/SqlSessionFactoryBean.java
+++ DAY 05/MybatisProject/src/main/java/com/springbook/biz/util/SqlSessionFactoryBean.java
@@ -12,7 +12,8 @@ public class SqlSessionFactoryBean {
 	static {
 		try {
 			if(sessionFactory == null) {
-				Reader reader = Resources.getResourceAsReader("sql-map-config.xml");	// sql-map-config.xml 입력 스트림을 생성
+				Reader reader = Resources.getResourceAsReader("sql-map-config.xml");	// sql-map-config.xml 파일을 읽기 위해 입력 스트림을 생성
+				// SqlSession 객체에 대한 공장 역할을 수행
 				sessionFactory = new SqlSessionFactoryBuilder().build(reader);	// 입력 스트림을 통해 sql-map-config.xml 파일 읽어 SqlSessionFactory 객체 생성 
 			}
 		} catch (Exception e) {
