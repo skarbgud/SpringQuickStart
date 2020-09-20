d2e2fa1ad5ecd319384794aed233c0b648d52198
Nam <sjarbgud@gmail.com>
2020-09-20 23:05:36 +0900

    (DAY5)CLASS05 - JPA 환경설정

--- DAY 05/JPAProject/src/main/java/com/springbook/biz/board/Board.java
+++ DAY 05/JPAProject/src/main/java/com/springbook/biz/board/Board.java
@@ -13,6 +13,41 @@ import javax.persistence.TemporalType;
  * Entity implementation class for Entity: Board
  *
  */
+/*
+	@Table : 엔티티 클래스를 정의할 때 엔티티 클래스와 매핑되는 테이블의 이름을 별도로 지정하지 않으면 엔티티 클래스 이름과 같은 테이블이 자동으로 매핑
+	속성 					설명
+	name 				매핑될 테이블 이름을 지정한다.
+	catalog				데이터베이스 카탈로그를 지정한다.
+	schema				데이터베이스 스키마를(schema)를 지정한다.
+	uniqueConstraints	결합 unique 제약조건을 지정하며, 여러 개의 칼럼이 결합되어 유일성을 보장해야 하는 경우 사용한다.
+	
+	ex) @Table(name="E_BOARD", uniqueConstraints={@UniqueConstraints(columnName={"SEQ", "WRITER"})}) -> SEQ, WRITER 두개의 칼럼을 결합했을 때 유일한 값만 유지
+	
+	
+	@Column : 엔티티 클래스의 변수와 테이블의 칼럼을 매핑할 때 사용. 일반적으로 엔티티 클래스의 변수 이름과 칼럼 이름이 다를 때 사용, 생략시 변수이름과 칼럼 이름을 동일하게 매핑
+	속성 					설명
+	name				칼럼 이름을 지정한다.(생략 시 프로퍼티명과 동일하게 매핑)
+	unique				unique 제약조건을 추가한다(기본값:false)
+	nullable			null 상태 허용 여부를 설정한다.(기본값:false)
+	insertable			입력 SQL 명령어를 자동으로 생성할 때 이 칼럼을 포함할 것인지를 지정한다.(기본값:true)
+	updatable			수정 SQL 명령어를 자동으로 생성할 때 이 칼럼을 포함할 것인지를 지정한다.(기본값:true)
+	columnDefinition	이 칼럼에 대한 DDL 문을 직접 설정한다.
+	length				문자열 타입의 칼럼 길이를 지정한다.(기본값:255) 길이제한
+	precision			숫자 타입의 전체 자리수를 지정한다(기본값:0)
+	scale				숫자 타입의 소수점 자리수를 지정한다.(기본값:0)
+	
+	@GeneratedValue : @Id로 지정된 식별자 필드에 Primary Key 값을 생성하여 저장할 때 사용한다.
+	속성						설명
+	strategy				자동 생성 유형을 지정한다.(GenerationType 지정)
+	generator 				이미 생성된 Generator 이름을 지정한다.
+	
+	@Transient : 엔티티 클래스 내의 특정 변수를 영속 필드에서 제외할 때 사용 -> 제외할 변수들
+	
+	@Temporal : java.util.Date 타입의 날짜 데이터를 매핑할 때 사용한다. 이 때 TemporalType을 이용하면 출력되는 날짜의 형식을 지정할 수 있다.
+	Temporal.TIME은 시간 정보만 출력한다. Temporal.TIMESTAMP는 날짜와 시간 정보를 모두 출력한다.
+ */
+
+
 @Entity	// @Entity가 설정된 클래스를 엔티티 클래스라고 하며, @Entity가 붙은 클래스는 테이블과 매핑된다.
 @Table(name = "BOARD")	// 엔티티와 관련된 테이블을 매핑한다. name 속성을 사용하며 BOARD 테이블과 매핑했는데 클래스 이름이 테이블 이름과 매핑된다.
 public class Board{
--- DAY 05/JPAProject/src/main/java/com/springbook/biz/board/BoardServiceClient.java
+++ DAY 05/JPAProject/src/main/java/com/springbook/biz/board/BoardServiceClient.java
@@ -9,6 +9,15 @@ import javax.persistence.Persistence;
 
 public class BoardServiceClient {
 	public static void main(String[] args) {
+		/*
+			JPA 구조
+			1. Persistence 클래스를 이용하여 영속성 유닛 정보가 저장된 JPA 메인 환경 설정 파일(persistence.xml)을 로딩한다.
+			2. 이 설정 정보를 바탕으로 EntityManager를 생성하는 공장 기능의 EntityManagerFactory 객체를 생성한다.
+			3. 이제 EntityManagerFactory로부터 필요한 EntityManager를 얻어서 사용
+		 */
+		
+		
+		
 		//	EntityManager 생성
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPAProject");
 		EntityManager em = emf.createEntityManager();
@@ -26,8 +35,18 @@ public class BoardServiceClient {
 			// 글 등록
 			em.persist(board);
 			
+			
+			/*
+				메소드 											기능 설명
+				persist(Object entity) 							엔티티를 영속화한다.(INSERT)
+				merge(Object entity) 							준영속 상태의 엔티티를 영속화한다.(UPDATE)
+				remove(Object entity) 							영속 상태의 엔티티를 제거한다.(DELETE)
+				find(Class<T> entityClass, Object primaryKey)	하나의 엔티티를 검색한다.(SELECT ONE)
+				createQuery(String qlString, Class<T> resultClass) JPQL에 해당하는 엔티티 목록을 검색한다.(SELECT LIST)
+			 */
+			
 			// 글 목록 조회
-			String jpql = "select b from Board b order by b.seq desc";
+			String jpql = "select b from Board b order by b.seq desc";	// JPQL(JPA 고유의 쿼리 구문)
 			List<Board> boardList = em.createQuery(jpql, Board.class).getResultList();
 			
 			for(Board brd : boardList) {
